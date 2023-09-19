95a5b1546e93a0cab96ca1d94e2c0798f2aa7ad0
suleiman gacheru <dretrevor8@gmail.com>
2023-09-19 21:12:10 +0300

    removed live demo

--- README.md
+++ README.md
@@ -106,14 +106,6 @@ After you're finished please remove all the comments and instructions!
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
-<!-- LIVE DEMO -->
-<!-- >
-## 🚀 Live Demo <a name="live-demo"></a>
-
-- [Live Demo Link](<replace-with-your-deployment-URL>)
-
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
-
 <!-- GETTING STARTED -->
 
 ## 💻 Getting Started <a name="getting-started"></a>
--- data.sql
+++ data.sql
@@ -1,2 +1,2 @@
 /* Populate database with sample data. */
- INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (0, 'Agumon', '2020-02-3', 0, TRUE, 10.23),(1, 'Gabumon', '2018-11-18', 2, TRUE, 8),(2, 'Pikachu', '2021-01-7', 1, FALSE, 15.04),(3, 'Devimon', '2017-5-12', 5, TRUE, 11);
+ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (0, 'Agumon', '2020-02-3', 0, TRUE, 10.23),(1, 'Gabumon', '2018-11-15', 2, TRUE, 8),(2, 'Pikachu', '2021-01-7', 1, FALSE, 15.04),(3, 'Devimon', '2017-5-12', 5, TRUE, 11);
