81689f7bc3810296c5ed2bce31d10626d8a89f3d
suleiman gacheru <dretrevor8@gmail.com>
2023-09-21 00:12:37 +0300

    add queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,32 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+
+delete from animals where date_of_birth > '2022-01-01';
+DELETE 1
+
+savepoint sp3;
+SAVEPOINT
+
+update animals set weight_kg = weight_kg * -1;
+UPDATE 10
+
+rollback to sp3;
+ROLLBACK
+
+update animals set weight_kg = weight_kg * -1 where weight_kg < 0;
+UPDATE 4
+commit;
+
+SELECT COUNT(*) AS total_animals FROM animals;
+
+select count(*) as animals_never_escape from animals where escape_attempts = 0;
+
+select avg(weight_kg) as average_weight from animals; 
+
+SELECT neutered, SUM(escape_attempts) AS total_escape_attempts FROM animals GROUP BY neutered ORDER BY total_escape_attempts DESC LIMIT 1;
+
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts FROM (SELECT species, COUNT(*) AS escape_attempts FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species) AS subquery GROUP BY species;
