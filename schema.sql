284bf0ec221a4790cc19e69cfb0a37e12301de59
suleiman yusuf <108791588+hetrox8@users.noreply.github.com>
2023-09-21 00:17:27 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,14 +1,4 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-     id INT PRIMARY KEY NOT NULL,
-     name varchar(30)
-     date_of_birth DATE,
-     escape_attempts INT,
-     neutered BOOLEAN,
-     weight_kg DECIMAL
-);
-
   alter table animals
   add column species varchar(45);
   ALTER TABLE
