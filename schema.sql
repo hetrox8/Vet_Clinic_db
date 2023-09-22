57c846767e77908a4b722e0a78b442ab26fd0fd3
suleiman yusuf <108791588+hetrox8@users.noreply.github.com>
2023-09-22 13:42:03 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,18 @@
 /* Database schema to keep the structure of entire database. */
-  alter table animals
-  add column species varchar(45);
-  ALTER TABLE
-  select * from animals;
+ 
+create table owners (
+id serial primary key,
+full_name varchar(35),
+age int not null
+ );
+
+create table species (
+id serial primary key,
+name varchar(30)
+ );
+
+alter table animals
+drop column species;
+
+alter table animals add column species_id int references species(id);
+alter table animals add column owner_id int references owners(id);
